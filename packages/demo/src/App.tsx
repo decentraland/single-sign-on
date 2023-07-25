65ea7942107c9c9103c36cae43bca8965dc0473b
Nando <24811313+fzavalia@users.noreply.github.com>
2023-07-25 16:36:59 -0300

    feat: WIP demo

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [id, setId] = useState(1);
+  const [key, setKey] = useState("");
+  const [value, setValue] = useState("");
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Single Sign On - Demo</h1>
+      </header>
+      <main>
+        <div style={{ display: "flex", flexDirection: "column", gap: ".5rem" }}>
+          <Input label="key" onChange={(e) => setKey(e.target.value)} />
+          <Input label="value" onChange={(e) => setValue(e.target.value)} />
+        </div>
+        <div style={{ display: "flex", gap: ".5rem", marginTop: ".5rem" }}>
+          <button
+            onClick={() => {
+              const cw = iframeRef.current?.contentWindow;
+
+              if (cw) {
+                cw.postMessage({ id, action: "set", key, value }, "*");
+              }
+            }}
+          >
+            SET
+          </button>
+          <button
+            onClick={() => {
+              const cw = iframeRef.current?.contentWindow;
+
+              if (cw) {
+                cw.postMessage({ id, action: "get", key }, "*");
+              }
+
+              setId((id) => id + 1);
+            }}
+          >
+            GET
+          </button>
+        </div>
+      </main>
+      <iframe style={{ marginTop: "1rem" }} ref={iframeRef} src="http://localhost:3001"></iframe>
     </>
-  )
+  );
 }
 
-export default App
+function Input({ label, onChange }: { label: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+  return (
+    <label style={{ display: "flex" }}>
+      <div style={{ width: 40 }}>{label}</div>
+      <input type="text" onChange={onChange} />
+    </label>
+  );
+}
+
+
+
+export default App;
