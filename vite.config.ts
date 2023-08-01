ed1cba913a090ca4e71baf4289de87de4565a908
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-01 15:38:38 -0300

    feat: Make things simple

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from "vite";
+import { UserConfigExport, defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3001,
-  },
+export default defineConfig(({ command }) => {
+  const config: UserConfigExport = {
+    server: {
+      port: 3001,
+    },
+  };
+
+  if (command === "build" && !!process.env.CI) {
+    const packageJson = path.resolve(__dirname, "package.json");
+    const packageJsonContent = fs.readFileSync(packageJson, "utf-8");
+    const packageJsonData = JSON.parse(packageJsonContent);
+
+    config.base = `https://cdn.decentraland.org/${packageJsonData.name}/${packageJsonData.version}`;
+  }
+
+  return config;
 });
