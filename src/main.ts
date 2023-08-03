64d0f62dbfd244b82a98e1c5cc9bdaa7fa4209c0
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-03 16:38:05 -0300

    fix: Add comments

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,22 @@ import { Action, Payload, PostMessageExtraArgs } from "./types";
 // All decentraland vercel deployments (https://*-decentraland1.vercel.app)
 // All projects running on localhost (http://localhost:*)
 const allow = /(^https:\/\/.+(\.decentraland.(org|today|zone)|-decentraland1.vercel.app)$|http:\/\/localhost:)/;
+
+// What messages targeted to this iframe should include as event.data.target
+// Also used for the response so the client can identify the message along the id
 const expectedTarget = "single-sign-on";
 
 window.addEventListener("message", (event: MessageEvent<Payload | null | undefined>) => {
   const { origin, data } = event;
 
+  // Ignore if there is no data in the message
   if (!data) {
     return;
   }
 
   const { target, id, action, user, identity } = data;
 
+  // Ignore if target is not the expected one
   if (target !== expectedTarget) {
     return;
   }
@@ -32,24 +37,32 @@ window.addEventListener("message", (event: MessageEvent<Payload | null | undefin
   };
 
   try {
+    // Fail if the origin is not allowed
     if (!allow.test(origin)) {
       throw new Error(`Origin is not accepted`);
     }
 
+    // Fail if the message does not have an id
+    // Required as it is used by the client to identify the response
     if (!id) {
       throw new Error("Id is required");
     }
 
+    // Fail if the provided action is not supported
     if (!Object.values(Action).includes(action)) {
       throw new Error(`Action is not supported`);
     }
 
+    // Fail if the user is not a string
+    // Users should be the string of an ethereum address
     if (typeof user !== "string") {
       throw new Error("User is required and must be a string");
     }
 
+    // Lowercasing the user to prevent issues with case sensitivity
     const lcUser = user.toLowerCase();
 
+    // The key used to store the identity in localStorage
     const key = `single-sign-on-${lcUser}`;
 
     switch (action as Action) {
