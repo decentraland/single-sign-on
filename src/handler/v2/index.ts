0f051553eb0b0de2370b70b2f89c7c0b10d3140d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-11-06 11:19:49 -0300

    fix: Refactor

--- src/handler/v2/index.ts
+++ src/handler/v2/index.ts
@@ -13,47 +13,33 @@ export const handler = (event: MessageEvent<ClientMessage>) => {
     return;
   }
 
+  let response: ServerMessage = {
+    target: SINGLE_SIGN_ON_TARGET,
+    id: event.data.id,
+    ok: true,
+    action: event.data.action,
+  };
+
   switch (event.data.action) {
     case Action.SET_CONNECTION_DATA: {
-      console.log("SSO received SET_CONNECTION_DATA");
-
-      let response: Omit<ServerMessage, "target"> = {
-        id: event.data.id,
-        action: Action.SET_CONNECTION_DATA,
-        ok: true,
-      };
-
       try {
         LocalStorageUtils.setConnectionData(event.data.payload as ConnectionData | null);
       } catch (e) {
         response.ok = false;
         response.payload = (e as Error).message;
       }
-
-      postMessage(response, event.origin);
-
+      window.parent.postMessage(response, event.origin);
       break;
     }
 
     case Action.GET_CONNECTION_DATA: {
-      console.log("SSO received GET_CONNECTION_DATA");
       const connectionData = LocalStorageUtils.getConnectionData();
-      postMessage(
-        { id: event.data.id, action: Action.GET_CONNECTION_DATA, ok: true, payload: connectionData },
-        event.origin
-      );
+      response.payload = connectionData;
+      window.parent.postMessage(response, event.origin);
       break;
     }
 
     case Action.SET_IDENTITY: {
-      console.log("SSO received SET_IDENTITY");
-
-      let response: Omit<ServerMessage, "target"> = {
-        id: event.data.id,
-        action: Action.SET_IDENTITY,
-        ok: true,
-      };
-
       try {
         const { address, identity } = event.data.payload as IdentityPayload;
         LocalStorageUtils.setIdentity(address, identity);
@@ -61,29 +47,19 @@ export const handler = (event: MessageEvent<ClientMessage>) => {
         response.ok = false;
         response.payload = (e as Error).message;
       }
-
-      postMessage(response, event.origin);
-
+      window.parent.postMessage(response, event.origin);
       break;
     }
 
     case Action.GET_IDENTITY: {
-      console.log("SSO received GET_IDENTITY");
       const identity = LocalStorageUtils.getIdentity(event.data.payload as string);
-      postMessage({ id: event.data.id, action: Action.GET_IDENTITY, ok: true, payload: identity }, event.origin);
+      response.payload = identity;
+      window.parent.postMessage(response, event.origin);
       break;
-    }
-
-    default: {
-      console.log("SSO received unsupported action:", event.data.action);
     }
   }
 };
 
 export function init() {
-  postMessage({ id: -1, action: Action.INIT, ok: true }, "*");
-}
-
-function postMessage(message: Omit<ServerMessage, "target">, origin: string) {
-  window.parent.postMessage({ target: SINGLE_SIGN_ON_TARGET, ...message }, origin);
+  window.parent.postMessage({ target: SINGLE_SIGN_ON_TARGET, id: -1, action: Action.INIT, ok: true }, "*");
 }
